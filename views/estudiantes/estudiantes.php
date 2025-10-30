2d80de116df88425d533c8f13e5bc85ad80ebeac
Maikol Vargas <msvargas@uniboyaca.edu.co>
2025-10-29 20:10:18 -0500

    arreglo de errores al ejecutar

--- views/estudiantes/estudiantes.php
+++ views/estudiantes/estudiantes.php
@@ -2,8 +2,11 @@
 require __DIR__ . '/../../controllers/EstudiantesController.php';
 use App\Controllers\EstudiantesController;
 
+$controller = new EstudiantesController();
+$estudiantes = $controller->queryAllEstudiantes(); 
+
+// Si llega una solicitud POST (por ejemplo, borrar estudiante)
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $controller = new EstudiantesController();
     $resultado = $controller->deleteEstudiante($_POST);
 
     if ($resultado === true) {
@@ -27,6 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <h1>Lista de estudiantes</h1>
     <a href="estudiante-form.php">Crear nuevo</a>
     <a href="../home.php">Volver</a>
+
     <table border="1" cellpadding="5">
         <thead>
             <tr>
@@ -34,34 +38,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <th>Nombre</th>
                 <th>Email</th>
                 <th>Programa</th>
+                <th>Acción</th>
             </tr>
         </thead>
         <tbody>
-            <?php
-            foreach ($estudiantes as $e) {
-                echo '<tr>';
-                echo '  <td>' . $e->get('codigo') . '</td>';
-                echo '  <td>' . $e->get('nombre') . '</td>';
-                echo '  <td>' . $e->get('email') . '</td>';
-                echo '  <td>' . $e->get('programa') . '</td>';
-                echo '  <td>';
-                echo '      <button onclick="onClickBorrar(' . $e->get('codigo') . ')">';
-                echo '          <img src="../../public/res/borrar.svg" alt="Borrar" width="30px">';
-                echo '      </button>';
-                echo '  </td>';
-                echo '  <td>';
-                echo '      <button>';   
-                echo '      <a href="estudiante-form.php?cod=' . $e->get('codigo') . '">';
-                echo '          <img src="../../public/res/modificar.svg" alt="modificar" width="30px">';
-                echo '      </a>';
-                echo '      </button>';
-                echo '  </td>';
-                echo '</tr>';
-            }
-            ?>
+            <?php if (!empty($estudiantes)) : ?>
+                <?php foreach ($estudiantes as $e): ?>
+                    <tr>
+                        <td><?= $e->get('codigo') ?></td>
+                        <td><?= $e->get('nombre') ?></td>
+                        <td><?= $e->get('email') ?></td>
+                        <td><?= $e->get('programa') ?></td>
+                        <td>
+                            <button onclick="onClickBorrar('<?= $e->get('codigo') ?>')">
+                                <img src="../../public/res/borrar.svg" alt="Borrar" width="30px">
+                            </button>
+                            <button>
+                                <a href="estudiante-form.php?cod=<?= $e->get('codigo') ?>">
+                                    <img src="../../public/res/modificar.svg" alt="Modificar" width="30px">
+                                </a>
+                            </button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <tr><td colspan="5">No hay estudiantes registrados.</td></tr>
+            <?php endif; ?>
         </tbody>
     </table>
-    
-    <script src="../../public/js/estudiante.php"></script>
+    <script src="../../public/js/estudiante.js"></script>
 </body>
 </html>
